a928a435b7efaee52483c11c3cfd5dd3629df1eb
arnobg <78933256+arnobg@users.noreply.github.com>
2023-02-02 08:26:50 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "aws" {
   # Configuration options
-  region     = "us-east-1"
+  region     = "eu-west-1"
   access_key = "<AWS ACCESS KEY>"
   secret_key = "<AWS SECRET KEY>"
 }
